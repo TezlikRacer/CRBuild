66467caec21373a8fabdde42df7227a500c186b4
Paul Joseph Canavan <ssbmotobug@gmail.com>
2023-11-21 21:46:20 -0500

    Large stage setup update

--- Build/Project+/Source/Extras/More.asm
+++ Build/Project+/Source/Extras/More.asm
@@ -5,3 +5,67 @@
 
 #.include source/Extras/USBGecko.asm 		# Adds support for Gecko codes passed in by a USB Gecko
 
+
+############################################################################
+Set random stagelist based on Code Menu Stagelist setting (PMBR) [Bird]
+############################################################################
+* 20523400 00000000 # If Code Menu stagelist is set to 0
+* 20523310 00000000 # If 80523310 is 00000000
+* 04523310 DEADBEEF # Set it to DEADBEEF
+* 04523320 00000000 # Set all other values to 00000000
+* 04523330 00000000
+* 04523340 00000000
+* 04523350 00000000
+* 04523360 00000000
+* 42000000 90000000 #Set base address to 90000000
+* 0417BE74 11280017 # Brawl stages
+* 0417BE70 00020800 # Melee stages
+* E0000000 80008000
+
+############################################################################
+Set random stagelist based on Code Menu Stagelist setting (Middle 3) [Bird]
+############################################################################
+* 20523400 00000001 # If Code Menu stagelist is set to 1
+* 20523320 00000000 # If 80523330 is 00000000
+* 04523320 DEADBEEF # Set it to DEADBEEF
+* 04523310 00000000 # Set all other values to 00000000
+* 04523330 00000000
+* 04523340 00000000
+* 04523350 00000000
+* 04523360 00000000
+* 42000000 90000000 #Set base address to 90000000
+* 0417BE74 04000001 # Brawl stages
+* 0417BE70 00020000 # Melee stages
+* E0000000 80008000
+
+############################################################################
+Set random stagelist based on Code Menu Stagelist setting (2023 Singles) [Bird, Moto]
+############################################################################
+* 20523400 00000002 # If Code Menu stagelist is set to 2
+* 20523330 00000000 # If 80523340 is 00000000
+* 04523330 DEADBEEF # Set it to DEADBEEF
+* 04523310 00000000 # Set all other values to 00000000
+* 04523320 00000000
+* 04523340 00000000
+* 04523350 00000000
+* 04523360 00000000
+* 42000000 90000000 #Set base address to 90000000
+* 0417BE74 04001909 # Brawl stages
+* 0417BE70 00021000 # Melee stages
+* E0000000 80008000
+
+############################################################################
+Set random stagelist based on Code Menu Stagelist setting (2023 Doubles) [Bird, Moto]
+############################################################################
+* 20523400 00000003 # If Code Menu stagelist is set to 3
+* 20523340 00000000 # If 80523320 is 00000000
+* 04523340 DEADBEEF # Set it to DEADBEEF
+* 04523310 00000000 # Set all other values to 00000000
+* 04523320 00000000
+* 04523330 00000000
+* 04523350 00000000
+* 04523360 00000000
+* 42000000 90000000 #Set base address to 90000000
+* 0417BE74 0400080B # Brawl stages
+* 0417BE70 00021000 # Melee stages
+* E0000000 80008000
